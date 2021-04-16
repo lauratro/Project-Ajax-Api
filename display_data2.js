4c5a0c71ed1e1865f3a49cb3c5bb7712eeba540d
Laura <65158950+lauratro@users.noreply.github.com>
2021-04-16 19:27:48 +0200

    1st try upload  value query param

--- display_data2.js
+++ display_data2.js
@@ -1,66 +1,68 @@
 let dataResult = document.getElementById("dataResult");
 //table
 let tableResult = document.getElementById("tableResult");
-let prevBtn = document.getElementById("prev");
+let tot = document.getElementById("tot");
 let succBtn = document.getElementById("succ");
-let pageNumber = 1;
+let prevBtn = document.getElementById("prev");
 let currentPage = document.getElementById("page");
-
-var i = 2;
+let pageNumber = 1;
 let y = 1;
-let f;
-let url1 = ``;
-function pageChanger() {
-  succBtn.addEventListener("click", function increment() {
-    y = i++;
-    currentPage.innerHTML = y;
-    console.log(y);
-    url1 = `https://api.punkapi.com/v2/beers?page=${y}`;
-
-    console.log(url1);
+//Table
+let displayResults = (data) => {
+  tableResult.classList.toggle("showTable");
+  var tab = "";
+  var num = "";
+  data.forEach((d) => {
+    num++;
+    tab += "<tr>";
+    tab += "<td>" + d.id + "</td>";
+    tab += "<td>" + d.name + "</td>";
+    tab += "<td>" + d.abv + "</td>";
+    tab += "</tr>";
   });
-  prevBtn.addEventListener("click", function increment() {
-    y = i--;
-    currentPage.innerHTML = y;
-    console.log(y);
-    url1 = `https://api.punkapi.com/v2/beers?page=${y}`;
+  dataResult.innerHTML = tab;
+};
+var btns = document.querySelectorAll("button");
+btns.forEach(function (btn) {
+  btn.addEventListener("click", updateAmount);
+});
+var num;
 
-    console.log(url1);
-  });
+function updateAmount() {
+  num = parseInt(document.getElementById("number").textContent.trim(), 10);
+  this.value === "minus" ? num-- : num++;
+  document.getElementById("number").textContent = num;
 
-  fetch(
-    url1,
-
-    {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-      },
-    }
-  )
-    .then((response) => response.json())
-    .then((data) => {
-      console.log("Success:", data);
-      data.sort(function (a, b) {
-        return a.id - b.id;
-      });
-
-      if (data.length > 0) {
-        tableResult.classList.toggle("showTable");
-        var tab = "";
-        var num = 0;
-        data.forEach((d) => {
-          num++;
-          tab += "<tr>";
-          tab += "<td>" + d.id + "</td>";
-          tab += "<td>" + d.name + "</td>";
-          tab += "<td>" + d.abv + "</td>";
-          tab += "</tr>";
-        });
-        dataResult.innerHTML = tab;
-      }
-    })
-    .catch((error) => {
-      console.error("Error:", error);
-    });
+  return num;
 }
+
+let pageChanger = (num) => {
+  var url = new URL("https://api.punkapi.com/v2/beers?");
+  var b = num;
+
+  url.searchParams.set("page", b);
+
+  console.log(url);
+  return url;
+};
+
+fetch(pageChanger(updateAmount()), {
+  method: "GET",
+  headers: {
+    "Content-Type": "application/json",
+  },
+})
+  .then((response) => response.json())
+  .then((data) => {
+    //  console.log("Success:", data);
+    data.sort(function (a, b) {
+      return a.id - b.id;
+    });
+    displayResults(data);
+    if (data) {
+      updateAmount();
+    }
+  })
+  .catch((error) => {
+    console.error("Error:", error);
+  });
