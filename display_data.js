4bc4fa005a19dbf60aada54f649e5a34f64fd644
Laura <65158950+lauratro@users.noreply.github.com>
2021-04-20 14:30:38 +0200

    Added select dropdown buttom with js

--- display_data.js
+++ display_data.js
@@ -1,49 +1,58 @@
-let dataResult = document.getElementById("dataResult");
-//table
+//Table Variables
 let tableResult = document.getElementById("tableResult");
+let bodyTable = document.getElementById("bodyTable");
 let tot = document.getElementById("tot");
-let succBtn = document.getElementById("succ");
-let prevBtn = document.getElementById("prev");
-let currentPage = document.getElementById("page");
-let pageNumber = 1;
-let y = 1;
-//radio
-/* let form2 = document.getElementById("form2");
-console.log("form2", form2);
-let trueBtn = document.getElementById("true");
-let falseBtn = document.getElementById("false");
-let submitRadio = document.getElementById("submit");
-let ciao = document.getElementById("ciao"); */
 
 //Table
 let displayResults = (data) => {
   tableResult.classList.toggle("showTable");
-  var tab = "";
-  var num = "";
+  let numData = "";
+  tot.innerHTML = data.length;
   data.forEach((d) => {
-    num++;
-    tab += "<tr>";
-    tab += "<td>" + d.id + "</td>";
-    tab += "<td>" + d.name + "</td>";
-    tab += "<td>" + d.abv + "</td>";
-    tab += "</tr>";
+    let tr = document.createElement("tr");
+    let td0 = document.createElement("td");
+    let td1 = document.createElement("td");
+    let td2 = document.createElement("td");
+    let td3 = document.createElement("td");
+
+    numData++;
+    td0.innerHTML = numData;
+    td1.innerHTML = d.id;
+    td2.innerHTML = d.name;
+    td3.innerHTML = d.abv;
+    tr.appendChild(td0);
+    tr.appendChild(td1);
+    tr.appendChild(td2);
+    tr.appendChild(td3);
+    bodyTable.appendChild(tr);
   });
-
-  dataResult.innerHTML = tab;
 };
-var btns = document.querySelectorAll("button");
-btns.forEach(function (btn) {
-  btn.addEventListener("click", updateAmount);
-});
-var num;
+// Select Dropdown Button
+let dropdownSelector = (data) => {
+  let dropdownGroup = document.getElementById("alcohol");
+  let filterAlcohol = [];
+  data.forEach((d) => {
+    filterAlcohol.push(d.abv); //create an array based on Alcohol value;
+  });
+  let uniqueArray = filterAlcohol.filter((item, pos) => {
+    //remove duplicates
+    return filterAlcohol.indexOf(item) == pos;
+  });
+  let integerValue = uniqueArray.filter((elem) => {
+    // select only integer numbers
+    if (Number.isInteger(elem)) {
+      return elem;
+    }
+  });
+  console.log("integerValue", integerValue);
+  integerValue.forEach((elem) => {
+    let option = document.createElement("option");
+    option.value = elem;
+    option.innerHTML = elem;
+    dropdownGroup.appendChild(option);
+  });
+};
 
-function updateAmount() {
-  num = parseInt(document.getElementById("number").textContent.trim(), 10);
-  this.value === "minus" ? num-- : num++;
-  document.getElementById("number").textContent = num;
-
-  return num;
-}
 let method = {
   method: "GET",
   headers: {
@@ -73,40 +82,7 @@ Promise.all([
     data.sort(function (a, b) {
       return a.abv - b.abv;
     });
-    function changeRadio() {
-      if (document.querySelector('input[name="alcohol"]')) {
-        document.querySelectorAll('input[name="alcohol"]').forEach((elem) => {
-          elem.addEventListener("change", function (event) {
-            var item = event.target.value;
-            console.log(item);
-            if (item == "one") {
-              let filtrato = data.filter((x, i) => {
-                return x.abv < 4.1 && x.abv > 0;
-              });
+    dropdownSelector(data);
 
-              console.log(filtrato);
-              displayResults(filtrato);
-            } else if (item == "four") {
-              let filtrato = data.filter((x) => {
-                return x.abv < 6.1 && x.abv > 4;
-              });
-
-              console.log(filtrato);
-              displayResults(filtrato);
-            } else if (item == "six") {
-              let filtrato = data.filter((x) => {
-                return x.abv > 6;
-              });
-
-              console.log(filtrato);
-              displayResults(filtrato);
-            }
-          });
-        });
-      }
-    }
-    changeRadio();
-
-    console.log(data);
     displayResults(data);
   });
--- display_data2.js
+++ display_data2.js
@@ -1,4 +1,4 @@
-/* let dataResult = document.getElementById("dataResult");
+let dataResult = document.getElementById("dataResult");
 //table
 let tableResult = document.getElementById("tableResult");
 let tot = document.getElementById("tot");
@@ -7,6 +7,14 @@ let prevBtn = document.getElementById("prev");
 let currentPage = document.getElementById("page");
 let pageNumber = 1;
 let y = 1;
+//radio
+/* let form2 = document.getElementById("form2");
+console.log("form2", form2);
+let trueBtn = document.getElementById("true");
+let falseBtn = document.getElementById("false");
+let submitRadio = document.getElementById("submit");
+let ciao = document.getElementById("ciao"); */
+
 //Table
 let displayResults = (data) => {
   tableResult.classList.toggle("showTable");
@@ -20,6 +28,7 @@ let displayResults = (data) => {
     tab += "<td>" + d.abv + "</td>";
     tab += "</tr>";
   });
+
   dataResult.innerHTML = tab;
 };
 var btns = document.querySelectorAll("button");
@@ -35,34 +44,69 @@ function updateAmount() {
 
   return num;
 }
-
-let pageChanger = (num) => {
-  var url = new URL("https://api.punkapi.com/v2/beers?");
-  var b = num;
-
-  url.searchParams.set("page", b);
-
-  console.log(url);
-  return url;
-};
-
-fetch(pageChanger(updateAmount()), {
+let method = {
   method: "GET",
   headers: {
     "Content-Type": "application/json",
   },
-})
-  .then((response) => response.json())
-  .then((data) => {
-    //  console.log("Success:", data);
-    data.sort(function (a, b) {
-      return a.id - b.id;
-    });
-    displayResults(data);
-    if (data) {
-      updateAmount();
-    }
+};
+Promise.all([
+  fetch("https://api.punkapi.com/v2/beers?page=1&per_page=80", method),
+  fetch("https://api.punkapi.com/v2/beers?page=2&per_page=80", method),
+  fetch("https://api.punkapi.com/v2/beers?page=3&per_page=80", method),
+  fetch("https://api.punkapi.com/v2/beers?page=4&per_page=80", method),
+  fetch("https://api.punkapi.com/v2/beers?page=5&per_page=80", method),
+])
+  .then(function (responses) {
+    // Get a JSON object from each of the responses
+    return Promise.all(
+      responses.map(function (response) {
+        return response.json();
+      })
+    );
   })
-  .catch((error) => {
-    console.error("Error:", error);
-  }); */
+  .then((data) => {
+    let newData = [].concat.apply([], data); //Concat nested data arrays
+
+    data = newData;
+    console.log("Success:", data);
+    data.sort(function (a, b) {
+      return a.abv - b.abv;
+    });
+    function changeRadio() {
+      if (document.querySelector('input[name="alcohol"]')) {
+        document.querySelectorAll('input[name="alcohol"]').forEach((elem) => {
+          elem.addEventListener("change", function (event) {
+            var item = event.target.value;
+            console.log(item);
+            if (item == "one") {
+              let filtrato = data.filter((x, i) => {
+                return x.abv < 4.1 && x.abv > 0;
+              });
+
+              console.log(filtrato);
+              displayResults(filtrato);
+            } else if (item == "four") {
+              let filtrato = data.filter((x) => {
+                return x.abv < 6.1 && x.abv > 4;
+              });
+
+              console.log(filtrato);
+              displayResults(filtrato);
+            } else if (item == "six") {
+              let filtrato = data.filter((x) => {
+                return x.abv > 6;
+              });
+
+              console.log(filtrato);
+              displayResults(filtrato);
+            }
+          });
+        });
+      }
+    }
+    changeRadio();
+
+    console.log(data);
+    displayResults(data);
+  });
