3eac0592c18fad30ca253152176ab6ba8f6f673f
Laura <65158950+lauratro@users.noreply.github.com>
2021-04-30 14:08:20 +0200

    Moved index.html in the root

--- js/display_data.js
+++ js/display_data.js
@@ -332,11 +332,11 @@ let method = {
   },
 };
 Promise.all([
-  fetch("https://api.punkapi.com/v2/beers?page=1&per_page=20", method),
-  fetch("https://api.punkapi.com/v2/beers?page=2&per_page=20", method),
-  fetch("https://api.punkapi.com/v2/beers?page=3&per_page=20", method),
-  fetch("https://api.punkapi.com/v2/beers?page=4&per_page=20", method),
-  fetch("https://api.punkapi.com/v2/beers?page=5&per_page=20", method),
+  fetch("https://api.punkapi.com/v2/beers?page=1&per_page=80", method),
+  fetch("https://api.punkapi.com/v2/beers?page=2&per_page=80", method),
+  fetch("https://api.punkapi.com/v2/beers?page=3&per_page=80", method),
+  fetch("https://api.punkapi.com/v2/beers?page=4&per_page=80", method),
+  fetch("https://api.punkapi.com/v2/beers?page=5&per_page=80", method),
 ])
   .then(function (responses) {
     // Get a JSON object from each of the responses
